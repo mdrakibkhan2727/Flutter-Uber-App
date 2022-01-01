a35fe6b39150d13a691cb03879389d8b152b18ef
Md Rakib Khan <mdrakibkhan2727@gmail.com>
2022-01-01 21:08:04 +0600

    initial

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:rider_app/AllScreens/mainscreen.dart';
+import 'package:rider_app/AllScreens/loginScreen.dart';
+import 'package:rider_app/AllScreens/registrationScreen.dart';
+
+void main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+DatabaseReference usersRef =FirebaseDatabase.instance.reference().child("users");
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Taxi Rider App',
+      theme: ThemeData(
+        fontFamily: "Brand Bold",
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: LoginScreen.idScreen,
+      routes:
+      {
+        RegistrationScreen.idScreen: (context)=>RegistrationScreen(),
+        LoginScreen.idScreen: (context)=>LoginScreen(),
+        MainScreen.idScreen: (context)=>MainScreen(),
+      },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
